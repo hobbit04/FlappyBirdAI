67eccad72159e1790f0411a156189755e6759644
hobbit04 <73837968+hobbit04@users.noreply.github.com>
2020-11-02 23:43:26 +0900

    Add files via upload

---
+++ Flappy_bird.py
@@ -0,0 +1,150 @@
+import pygame as py
+import sys, random
+
+
+def draw_floor():
+    screen.blit(floor_surface, (floor_x_pos, 450))
+    screen.blit(floor_surface, (floor_x_pos + 288, 450))
+
+
+def create_pipe():
+    random_pipe_pos = random.choice(pipe_height)
+    bottom_pipe = pipe_surface.get_rect(midtop=(288, random_pipe_pos))
+    top_pipe = pipe_surface.get_rect(midbottom=(288, random_pipe_pos-120))
+    return bottom_pipe, top_pipe
+
+
+def move_pipes(pipes):
+    for pipe in pipes:
+        pipe.centerx -= 3
+        if pipe.centerx <= -20:
+            pipes.remove(pipe)
+    return pipes
+
+
+def draw_pipes(pipes):
+    for pipe in pipes:
+        if pipe.bottom >= 512:
+            screen.blit(pipe_surface, pipe)
+        else:
+            flip_pipe = py.transform.flip(pipe_surface, False, True)  # x축 회전 : F, y축 회전 : T
+            screen.blit(flip_pipe, pipe)
+
+
+def check_collision(pipes):
+    for pipe in pipes:
+        if bird_rect.colliderect(pipe):
+            return False
+    if bird_rect.top <= -50 or bird_rect.bottom >= 450:
+            return False
+
+    return True
+
+
+def rotate_bird(bird):
+    new_bird = py.transform.rotozoom(bird, bird_movement * -3, 1)
+    return new_bird
+
+
+def bird_animation():
+    new_bird = bird_frames[bird_index]
+    new_bird_rect = new_bird.get_rect(center=(50, bird_rect.centery))
+    return new_bird, new_bird_rect
+
+
+def score_display():
+    score_surface = game_font.render(str(int(score)), True, (255, 255, 255))
+    score_rect = score_surface.get_rect(center=(144, 100))
+    screen.blit(score_surface, score_rect)
+
+
+py.init()
+screen = py.display.set_mode((288, 512))
+clock = py.time.Clock()
+game_font = py.font.Font('04B_19.TTF', 40)
+
+# Game Variables
+gravity = 0.25
+bird_movement = 0
+game_active = True
+score = 0
+high_score = 0
+
+bg_surface = py.image.load('assets/background-day.png').convert()
+floor_surface = py.image.load('assets/base.png').convert()
+floor_x_pos = 0
+
+bird_downflap = py.image.load('assets/bluebird-downflap.png').convert_alpha()
+bird_midflap = py.image.load('assets/bluebird-midflap.png').convert_alpha()
+bird_upflap = py.image.load('assets/bluebird-upflap.png').convert_alpha()
+bird_frames = [bird_downflap, bird_midflap, bird_upflap]
+bird_index = 0
+bird_surface = bird_frames[bird_index]
+bird_rect = bird_surface.get_rect(center=(50, 256))
+
+BIRDFLAP = py.USEREVENT + 1
+py.time.set_timer(BIRDFLAP, 200)
+
+# bird_surface = py.image.load('assets/bluebird-midflap.png').convert_alpha()
+# bird_rect = bird_surface.get_rect(center=(50, 256))
+
+pipe_surface = py.image.load('assets/pipe-green.png').convert()
+pipe_list = []
+SPAWNPIPE = py.USEREVENT
+py.time.set_timer(SPAWNPIPE, 1200)
+pipe_height = [200, 300, 400]
+
+
+while True:
+    for event in py.event.get():
+        if event.type == py.QUIT:
+            py.quit()
+            sys.exit()
+        if event.type == py.KEYDOWN:
+            if event.key == py.K_SPACE and game_active:
+                bird_movement = 0
+                bird_movement -= 6
+
+            if event.key == py.K_SPACE and not game_active:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (50, 256)
+                bird_movement = 0
+                bird_movement -= 6
+                score = 0
+
+        if event.type == SPAWNPIPE:
+            pipe_list.extend(create_pipe())
+
+        if event.type == BIRDFLAP:
+            if bird_index < 2:
+                bird_index += 1
+            else:
+                bird_index = 0
+
+            bird_surface, bird_rect = bird_animation()
+
+    screen.blit(bg_surface, (0, 0))
+
+    if game_active:
+        # Bird
+        bird_movement += gravity
+        rotated_bird = rotate_bird(bird_surface)
+        bird_rect.centery += bird_movement
+        screen.blit(rotated_bird, bird_rect)
+        game_active = check_collision(pipe_list)
+        # Pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
+        score += 0.01
+
+        score_display()
+
+    # Floor
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -288:
+        floor_x_pos = 0
+
+    py.display.update()
+    clock.tick(60)
